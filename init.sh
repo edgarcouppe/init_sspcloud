e68eb236512098ed1909c9b29c0197ea63919280
edgarcouppe <edgar.couppe@gmail.com>
2026-03-15 22:57:25 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo apt-get update && sudo apt-get install -y tesseract-ocr tesseract-ocr-eng
+sudo apt-get update && sudo apt-get install -y tesseract-ocr tesseract-ocr-eng poppler-utils
