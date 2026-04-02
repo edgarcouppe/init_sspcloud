857f9822178d7c5ae4e7e6634023f22348bc5a04
edgarcouppe <edgar.couppe@gmail.com>
2026-04-02 08:58:28 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 sudo apt-get update && sudo apt-get install -y tesseract-ocr tesseract-ocr-eng poppler-utils tesseract-ocr-chi-sim tesseract-ocr-chi-tra
+pip uninstall opencv-python -y
+pip install opencv-python-headless pymupdf pytesseract paddlepaddle paddleocr
