4c682472a4d7123fd180513567e6cae4974dda72
edgarcouppe <edgar.couppe@gmail.com>
2026-04-23 14:01:16 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sudo apt-get update
-apt-get install -y libgl1
+sudo apt-get update && sudo apt-get install -y tesseract-ocr tesseract-ocr-eng tesseract-ocr-chi-sim poppler-utils libgl1
 pip uninstall opencv-python -y
-pip install opencv-python-headless pymupdf paddlepaddle paddleocr
+pip install opencv-python-headless
